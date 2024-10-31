57ae7326856e0ffae41c27a63a9c4cf142bf3012
Eugene <doodleoocrypto@gmail.com>
2024-10-31 19:51:43 +0300

    Update index.ts

--- api/src/index.ts
+++ api/src/index.ts
@@ -4,7 +4,7 @@ import { Command } from 'commander';
 import Bun from 'bun';
 import toml from 'toml';
 import fs from 'fs';
-
+import { getNodesLocations } from './location.handler';
 const program = new Command();
 
 type Router = {
@@ -37,9 +37,9 @@ program
 
     // Define caching helper function
     function withCache(key: string, fetchFunction: () => Promise<any>) {
-      return async function () {
+      return async function (fromCache = true) {
         const cachedData = await storage.getItem(key);
-        if (cachedData) {
+        if (cachedData && fromCache) {
           console.log(`Using cached ${key}`);
           return cachedData;
         }
@@ -51,53 +51,57 @@ program
     }
 
     // Functions with cache
-    const getValidators = withCache('validators', () => q.query_validators_data());
-    const getEpoch = q.query_epoch.bind(q);
-    const getEffectiveSupply = withCache('effective_supply', () => q.effective_native_supply());
-    const getActiveVotingPower = withCache('active_voting_power', () => q.total_active_voting_power());
-    const getPGFparams = withCache('pgf', () => q.pgf_params());
-    const getStakingRewardsRate = withCache('staking_rewards_rate', () => q.staking_rewards_rate());
-    const getTotalStake = withCache('total_stake', () => q.total_stake());
-    const getTotalNativeSupply = withCache('total_native_supply', () => q.total_native_supply());
-    const getCurrentBlock = async () => {
+    const handlers = {
+      getValidators: withCache('validators', () => q.query_validators_data()),
+      getEpoch: q.query_epoch.bind(q),
+      getEffectiveSupply: withCache('effective_supply', () => q.effective_native_supply()),
+      getActiveVotingPower: withCache('active_voting_power', () => q.total_active_voting_power()),
+      getPGFparams: withCache('pgf', () => q.pgf_params()),
+      getStakingRewardsRate: withCache('staking_rewards_rate', () => q.staking_rewards_rate()),
+      getTotalStake: withCache('total_stake', () => q.total_stake()),
+      getTotalNativeSupply: withCache('total_native_supply', () => q.total_native_supply()),
+      getNodesLocationsCached: withCache('nodes_locations', () => getNodesLocations(config)),
+      getCurrentBlock: async () => {
       const status = await fetch(`${rpc}/status`).then((res) => res.json());
       return status.result.sync_info.latest_block_height;
-    };
-    const getChainId = async () => {
+      },
+      getChainId: async () => {
       const status = await fetch(`${rpc}/status`).then((res) => res.json());
       return status.result.node_info.network;
+      },
     };
 
     // Define router with all endpoints
     const router: Router = {
-      '/validators': getValidators,
-      '/epoch': async () => ({ value: await getEpoch() }),
-      '/effective_supply': async () => ({ value: await getEffectiveSupply() }),
-      '/total_active_voting_power': async () => ({ value: await getActiveVotingPower() }),
-      '/pgf': async () => ({ value: await getPGFparams() }),
-      '/staking_rewards_rate': async () => ({ value: await getStakingRewardsRate() }),
-      '/total_stake': async () => ({ value: await getTotalStake() }),
-      '/current_block': async () => ({ value: await getCurrentBlock() }),
-      '/total_native_supply': async () => ({ value: await getTotalNativeSupply() }),
-      '/chain_id': async () => ({ value: await getChainId() }),
+      '/validators': handlers.getValidators,
+      '/epoch': async () => ({ value: await handlers.getEpoch() }),
+      '/effective_supply': async () => ({ value: await handlers.getEffectiveSupply() }),
+      '/total_active_voting_power': async () => ({ value: await handlers.getActiveVotingPower() }),
+      '/pgf': async () => ({ value: await handlers.getPGFparams() }),
+      '/staking_rewards_rate': async () => ({ value: await handlers.getStakingRewardsRate() }),
+      '/total_stake': async () => ({ value: await handlers.getTotalStake() }),
+      '/current_block': async () => ({ value: await handlers.getCurrentBlock() }),
+      '/total_native_supply': async () => ({ value: await handlers.getTotalNativeSupply() }),
+      '/chain_id': async () => ({ value: await handlers.getChainId() }),
       '/total_stake_percentage': async () => {
-        const totalStake = await getTotalStake();
-        const totalNativeSupply = await getTotalNativeSupply();
-        const percentage = (Number(totalStake) / Number(totalNativeSupply)) * 100;
-        return { value: percentage.toFixed(2) };
+      const totalStake = await handlers.getTotalStake();
+      const totalNativeSupply = await handlers.getTotalNativeSupply();
+      const percentage = (Number(totalStake) / Number(totalNativeSupply)) * 100;
+      return { value: percentage.toFixed(2) };
       },
+      '/nodes_locations': () => handlers.getNodesLocationsCached(),
     };
 
     // Initialize cache for all endpoints
     async function initializeCache() {
-      await Promise.all(Object.values(router).map((fn) => fn()));
+      await Promise.all(Object.values(handlers).map((fn) => fn()));
     }
     await initializeCache();
 
     // Start periodic cache updates
     function startPeriodicCacheUpdates(interval: number) {
       setInterval(async () => {
-        await Promise.all(Object.values(router).map((fn) => fn()));
+      await Promise.all(Object.values(handlers).map((fn) => fn(false)));
       }, interval);
     }
     startPeriodicCacheUpdates(cache_interval); // Use interval from config
